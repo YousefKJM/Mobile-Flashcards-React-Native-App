9288ec6a8cf96432a46e5b458633859e9d89d4f3
Yousef Majeed <uaso0of056@gmail.com>
2020-04-30 18:39:27 +0300

    link store,paper provider to app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,28 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Provider as PaperProvider } from "react-native-paper";
+import { Provider as StoreProvider } from "react-redux";
+import store from "./store";
+
+import { theme } from "./utils/theme";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+	return (
+		<StoreProvider store={store}>
+			<PaperProvider theme={theme}>
+				<View style={styles.container}>
+					<Text>Open up App.tsx to start working on your app!</Text>
+				</View>
+			</PaperProvider>
+		</StoreProvider>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
