91682086f423b26796380a70b9753c6036b0ef54
Yousef Majeed <uaso0of056@gmail.com>
2020-04-30 20:23:02 +0300

    refactor

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,3 +1,4 @@
 export { default as DeckList } from "./DeckList";
 export { default as DeckSingle } from "./DeckSingle";
 export { default as Quiz } from "./Quiz";
+export { default as DeckAdd } from "./DeckAdd";
