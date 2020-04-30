08c60ad81335f152a76fa1c4582e2aa782803934
Yousef Majeed <uaso0of056@gmail.com>
2020-04-30 20:31:59 +0300

    add react-navigation-stack

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,17 @@ import { StyleSheet, Text, View } from "react-native";
 import { Provider as PaperProvider } from "react-native-paper";
 import { Provider as StoreProvider } from "react-redux";
 import store from "./store";
-
+import Router from "./router";
 import { theme } from "./utils/theme";
 
 export default function App() {
 	return (
 		<StoreProvider store={store}>
 			<PaperProvider theme={theme}>
-				<View style={styles.container}>
+				{/* <View style={styles.container}>
 					<Text>Open up App.tsx to start working on your app!</Text>
-				</View>
+				</View> */}
+				<Router />
 			</PaperProvider>
 		</StoreProvider>
 	);
